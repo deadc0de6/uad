d9d23796cc3153b1ea55a539f7e3785ed8bd4dda
deadc0de6 <info@deadc0de.ch>
2022-06-05 23:16:11 +0200

    bump version

--- uad.go
+++ uad.go
@@ -26,7 +26,7 @@ import (
 
 const (
 	fCreationRights = 0666
-	version         = "0.7.0"
+	version         = "0.7.1"
 	// web endpoint for files
 	downloadPath = "files/"
 	// web endpoint for upload
